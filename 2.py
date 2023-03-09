432bc28e60d953e7a84924132b42252ee6595c99
devjadiya <121892398+devjadiya@users.noreply.github.com>
2023-03-09 20:46:08 +0530

    DAY-1

---
+++ 2.py
@@ -0,0 +1,5 @@
+# Python Indentation
+if 5 > 2:
+ print("five is greater than two!")
+# The number of spaces is up to you as a programmer,
+#  the most common use is four, but it has to be at least one.
---
+++ 4.py
@@ -0,0 +1,4 @@
+# Python indentation
+if 6 > 2:
+    print("six" is greater than two!")
+          print("six is greater than two!")
