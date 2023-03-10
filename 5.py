0396c3488d67825d02fe99c3c10bc45b10057a0f
devjadiya <121892398+devjadiya@users.noreply.github.com>
2023-03-10 22:55:56 +0530

    DAY-2

---
+++ 5.py
@@ -0,0 +1,8 @@
+# Variables in python
+x = 5
+y = "Hello, Universe!"
+
+print(x)
+print(y)
+
+# Comments in python is used by hash symbol
---
+++ 6.py
@@ -0,0 +1,7 @@
+# Multi line comment
+"""
+This is the example of 
+mutltiline comment
+in python
+"""
+print("Python is amazing")
