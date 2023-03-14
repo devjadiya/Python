6edaae172f3d5d77aee7d692a09005980f1e16f9
devjadiya <121892398+devjadiya@users.noreply.github.com>
2023-03-14 23:36:03 +0530

    DAY-6

---
+++ 21.py
@@ -0,0 +1,8 @@
+# Python numbers
+# int ,float,complex
+x = 5454
+y = 32.324
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
---
+++ 22.py
@@ -0,0 +1,23 @@
+# Type Conversation
+x = 1
+y = 3.4
+z = 4j
+
+# Convert from int to float:
+a = float(x)
+
+# Convert from float to int:
+b = int(y)
+
+# Convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Note: You cannot convert complex numbers into another number type.
